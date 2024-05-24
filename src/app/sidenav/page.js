f4edb519442ef1cb6426f167a3fde131763a54e9
dhananjay <dhananjaydogne@gmail.com>
2024-05-24 14:28:54 +0530

    line grapg

--- src/app/sidenav/page.js
+++ src/app/sidenav/page.js
@@ -11,6 +11,9 @@ const SideNav = () => {
                 <li className='py-2 pl-2 rounded-full navlist  hover:bg-gray-800'>
                     <Link href="/dashboard"><p>Map</p></Link>
                 </li>
+                <li className='py-2 pl-2 rounded-full navlist  hover:bg-gray-800'>
+                    <Link href="/linegraph"><p>LineGraph</p></Link>
+                </li>
             </ul>
         </div>
     );
